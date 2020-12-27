1e61086c723ec73b424e810d476b67e126614479
Jenkin <zhaojiancun@yunquna.com>
2020-12-27 15:25:26 +0800

    fix: repair lint

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import { AppContainer } from 'react-hot-loader';
+import { AppContainer } from 'react-hot-loader'; // eslint-disable-line
 
 const ReactRender = module.hot ? ReactDOM.render : ReactDOM.hydrate
 
